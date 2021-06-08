78ae988e489e9c85490f8709fbf98aaecc3914a3
tomas-rln <tomas.ezequiel.rln@gmail.com>
2021-06-08 16:30:46 -0300

    Transaction component added

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { TransactionDiv, TransactionAmount, TransactionConcept } from './styles'
+import TrendingDownTwoToneIcon from '@material-ui/icons/TrendingDownTwoTone'
+import TrendingUpTwoToneIcon from '@material-ui/icons/TrendingUpTwoTone'
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney'
 
-export const Transaction = () => {
+export const Transaction = ({ item }) => {
+  const icon = item.type === 'Egress' ? <TrendingDownTwoToneIcon color='secondary' />
+    : <TrendingUpTwoToneIcon style={{ color: 'green' }} />
   return (
-        <div>
-            This is a transaction
-        </div>
+        <TransactionDiv>
+            <TransactionAmount>
+              {icon}
+              <h4>
+                {item.amount}
+              </h4>
+              <AttachMoneyIcon fontSize="small" />
+            </TransactionAmount>
+
+            <TransactionConcept>
+              {item.concept}
+            </TransactionConcept>
+        </TransactionDiv>
   )
 }
--- src/components/Transaction/styles.js
+++ src/components/Transaction/styles.js
@@ -1,1 +1,29 @@
+import styled from 'styled-components'
 
+export const TransactionDiv = styled.div`
+    width:340px;
+    height: 35px;
+    align-items: center;
+    display: flex;
+    border: 2px black solid;
+    margin: 2px 0;
+    border-radius:5px;
+    justify-content: space-between;
+    background-color: #E7F6FF;
+    @media (min-width: 361px){
+        min-width: 50rem;
+        height: 50px;
+       
+    }
+`
+
+export const TransactionAmount = styled.div`
+    display: flex; 
+    align-items: center;
+    justify-content: center;
+    margin: 0 5px; 
+`
+
+export const TransactionConcept = styled.div`
+    margin: 0 5px; 
+`
