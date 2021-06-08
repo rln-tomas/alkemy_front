f0d6763e414e955834946948c9abab980c9194e2
tomas-rln <tomas.ezequiel.rln@gmail.com>
2021-06-08 16:33:24 -0300

    Home page added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import { Header } from '../../components/Header'
 import { TransactionList } from '../../components/TransactionList'
+import { GlobalDiv } from './styles'
+import { Helmet } from 'react-helmet'
+import data from '../../dataTest'
+
+const balance = (items) => {
+  let total = 0
+  for (const i in items) {
+    if (items[i].type === 'Egress') {
+      total -= items[i].amount
+    } else {
+      total += items[i].amount
+    }
+  }
+
+  return total
+}
 
 export const Home = () => {
+  const total = balance(data)
   return (
         <>
-            <Header />
-            <TransactionList />
+          <Helmet>
+            <title>Transaction Tracker - Home</title>
+            <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+          </Helmet>
+          <GlobalDiv >
+            <Header balance={total} />
+            <TransactionList transactions={data} />
+          </GlobalDiv>
+
         </>
   )
 }
