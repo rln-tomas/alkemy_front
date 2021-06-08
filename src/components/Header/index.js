242c312ca1cd2f224a9af1b72dd65ad3b9f2a4d8
tomas-rln <tomas.ezequiel.rln@gmail.com>
2021-06-08 16:28:29 -0300

    Added header and styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { HeaderDiv, Balance } from './styles'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+import { IconButton } from '@material-ui/core'
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney'
 
-export const Header = () => {
+export const Header = ({ balance }) => {
   return (
-        <h2>This is the header</h2>
+        <HeaderDiv>
+          <Balance>
+            <AttachMoneyIcon fontSize='large' style={{ color: 'white' }} />
+            {balance}
+          </Balance>
+          <IconButton style={{ color: 'white' }}>
+           <AddBoxIcon fontSize='large' />
+          </IconButton>
+        </HeaderDiv>
   )
 }
---
+++ src/components/Header/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const HeaderDiv = styled.div`
+    max-width: 340px;
+    background-color: #00A2FF;
+    min-width: 300px;
+    display: flex;
+    flex-direction: column;
+    max-height: 300px;
+    border: 2px black solid;
+    border-radius: 2px;
+    padding: 1rem;
+    justify-content: center;
+    align-items: center;
+
+    @media (min-width: 641px){
+        max-width: 800px;
+    }
+   
+`
+
+export const Balance = styled.h1`
+    color: white; 
+    display: flex;
+    align-items: center;
+`
