fbaafae8cf7a3f51abd6a94c7db1791795cdcbb3
tomas-rln <tomas.ezequiel.rln@gmail.com>
2021-06-08 16:32:10 -0300

    Styles for transaction list

---
+++ src/components/TransactionList/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ListDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto; 
+`
